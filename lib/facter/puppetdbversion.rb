e493324eefe0249842d721c83161181a7c28f7e0
Baptiste Grenier <bgrenier@gnubila.fr>
2014-07-21 18:39:52 +0200

    puppetdbversion fact: fix version extraction.

--- lib/facter/puppetdbversion.rb
+++ lib/facter/puppetdbversion.rb
@@ -8,7 +8,7 @@ Facter.add("puppetdbversion") do
     if File.exists?(versionfile) and (File.stat(versionfile).mtime.to_i + (12*60*60) >= Time.now().to_i)
       %x{cat #{versionfile}}.chomp.to_f
     else
-      output = %x{/usr/bin/java -jar /usr/share/puppetdb/puppetdb.jar version 2>/dev/null | grep '^version'}
+      output = %x{/usr/bin/java -cp /usr/share/puppetdb/puppetdb.jar clojure.main -m com.puppetlabs.puppetdb.core version 2>/dev/null | grep '^version'}
       output.chomp!
       if output.length >0
         val = output.split('=')[-1]
