152a998affe4fb527057641181b570a333725796
Martin Alfke <ma@example42.com>
2015-08-03 16:31:33 +0200

    rspec tests
    - run bundle exec rake instead of rake on travis
    - use new travis architecture
    - pin rspec to 3.1.x on ruby 1.8.x
    - remove deprecations from spec tests

--- spec/classes/puppetdb_spec.rb
+++ spec/classes/puppetdb_spec.rb
@@ -4,7 +4,7 @@ describe 'puppetdb' do
 
   let(:title) { 'puppetdb' }
   let(:node) { 'rspec.example42.com' }
-  let(:facts) { { :ipaddress => '10.42.42.42',  :puppetdbversion => 1.40 } }
+  let(:facts) { { :ipaddress => '10.42.42.42',  :puppetdbversion => '1.40', :operatingsystemrelease => '6.6' } }
 
   describe 'Test standard installation' do
     it { should contain_package('puppetdb').with_ensure('present') }
@@ -27,12 +27,10 @@ describe 'puppetdb' do
     it { should contain_service('puppetdb').with_enable('true') }
     it { should contain_file('puppetdb.conf').with_ensure('present') }
     it 'should monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdb_process').with_enable(true)
     end
     it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdb_tcp_42').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('puppetdb_tcp_42').with_enable(true)
     end
   end
 
@@ -44,12 +42,10 @@ describe 'puppetdb' do
     it 'should not enable at boot Service[puppetdb]' do should contain_service('puppetdb').with_enable('false') end
     it 'should remove puppetdb configuration file' do should contain_file('puppetdb.conf').with_ensure('absent') end
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdb_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdb_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('puppetdb_tcp_42').with_enable(false)
     end
   end
 
@@ -61,12 +57,10 @@ describe 'puppetdb' do
     it 'should not enable at boot Service[puppetdb]' do should contain_service('puppetdb').with_enable('false') end
     it { should contain_file('puppetdb.conf').with_ensure('present') }
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdb_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdb_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('puppetdb_tcp_42').with_enable(false)
     end
   end
 
@@ -79,8 +73,7 @@ describe 'puppetdb' do
     it 'should not enable at boot Service[puppetdb]' do should contain_service('puppetdb').with_enable('false') end
     it { should contain_file('puppetdb.conf').with_ensure('present') }
     it 'should not monitor the process locally' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdb_process').with_enable(false)
     end
   end 
 
@@ -88,12 +81,10 @@ describe 'puppetdb' do
     let(:params) { {:template => "puppetdb/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'puppetdb.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('puppetdb.conf').with_content(/fqdn: rspec.example42.com/)
     end
     it 'should generate a template that uses custom options' do
-      content = catalogue.resource('file', 'puppetdb.conf').send(:parameters)[:content]
-      content.should match "value_a"
+      should contain_file('puppetdb.conf').with_content(/value_a/)
     end
 
   end
@@ -102,24 +93,20 @@ describe 'puppetdb' do
     let(:params) { {:source => "puppet://modules/puppetdb/spec" , :source_dir => "puppet://modules/puppetdb/dir/spec" , :source_dir_purge => true } }
 
     it 'should request a valid source ' do
-      content = catalogue.resource('file', 'puppetdb.conf').send(:parameters)[:source]
-      content.should == "puppet://modules/puppetdb/spec"
+      should contain_file('puppetdb.conf').with_source("puppet://modules/puppetdb/spec")
     end
     it 'should request a valid source dir' do
-      content = catalogue.resource('file', 'puppetdb.dir').send(:parameters)[:source]
-      content.should == "puppet://modules/puppetdb/dir/spec"
+      should contain_file('puppetdb.dir').with_source("puppet://modules/puppetdb/dir/spec")
     end
     it 'should purge source dir if source_dir_purge is true' do
-      content = catalogue.resource('file', 'puppetdb.dir').send(:parameters)[:purge]
-      content.should == true
+      should contain_file('puppetdb.dir').with_purge(true)
     end
   end
 
   describe 'Test customizations - custom class' do
     let(:params) { {:my_class => "puppetdb::spec" } }
     it 'should automatically include a custom class' do
-      content = catalogue.resource('file', 'puppetdb.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('puppetdb.conf').with_content(/fqdn: rspec.example42.com/)
     end
   end
 
@@ -131,8 +118,7 @@ describe 'puppetdb' do
     let(:params) { {:service_autorestart => "no" } }
 
     it 'should not automatically restart the service, when service_autorestart => false' do
-      content = catalogue.resource('file', 'puppetdb.conf').send(:parameters)[:notify]
-      content.should be_nil
+      should contain_file('puppetdb.conf').with_notify(nil)
     end
   end
 
@@ -140,8 +126,7 @@ describe 'puppetdb' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
 
     it 'should generate a puppi::ze define' do
-      content = catalogue.resource('puppi::ze', 'puppetdb').send(:parameters)[:helper]
-      content.should == "myhelper"
+      should contain_puppi__ze('puppetdb').with_helper("myhelper")
     end
   end
 
@@ -149,8 +134,7 @@ describe 'puppetdb' do
     let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
 
     it 'should generate monitor defines' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('puppetdb_process').with_tool("puppi")
     end
   end
 
@@ -158,8 +142,7 @@ describe 'puppetdb' do
     let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :https_port => "42" } }
 
     it 'should generate correct firewall define' do
-      content = catalogue.resource('firewall', 'puppetdb_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('puppetdb_tcp_42').with_tool("iptables")
     end
   end
 
@@ -167,61 +150,54 @@ describe 'puppetdb' do
     let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :https_port => "42" , :protocol => 'tcp' } }
 
     it 'should generate monitor resources' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('puppetdb_process').with_tool("puppi")
     end
     it 'should generate firewall resources' do
-      content = catalogue.resource('firewall', 'puppetdb_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('puppetdb_tcp_42').with_tool("iptables")
     end
     it 'should generate puppi resources ' do 
-      content = catalogue.resource('puppi::ze', 'puppetdb').send(:parameters)[:ensure]
-      content.should == "present"
+      should contain_puppi__ze('puppetdb').with_ensure("present")
     end
   end
 
   describe 'Test params lookup' do
-    let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => 1.40 } }
+    let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => '1.40', :operatingsystemrelease => '6.6' } }
     let(:params) { { :https_port => '42' } }
 
     it 'should honour top scope global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdb_process').with_enable(true)
     end
   end
 
   describe 'Test params lookup' do
-    let(:facts) { { :puppetdb_monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => 1.40 } }
+    let(:facts) { { :puppetdb_monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => '1.40', :operatingsystemrelease => '6.6' } }
     let(:params) { { :https_port => '42' } }
 
     it 'should honour module specific vars' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdb_process').with_enable(true)
     end
   end
 
   describe 'Test params lookup' do
-    let(:facts) { { :monitor => false , :puppetdb_monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => 1.40 } }
+    let(:facts) { { :monitor => false , :puppetdb_monitor => true , :ipaddress => '10.42.42.42', :puppetdbversion => '1.40', :operatingsystemrelease => '6.6' } }
     let(:params) { { :https_port => '42' } }
 
     it 'should honour top scope module specific over global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdb_process').with_enable(true)
     end
   end
 
   describe 'Test params lookup' do
-    let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42', :puppetdbversion => 1.40 } }
+    let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42', :puppetdbversion => '1.40', :operatingsystemrelease => '6.6' } }
     let(:params) { { :monitor => true , :firewall => true, :https_port => '42' } }
 
     it 'should honour passed params over global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdb_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdb_process').with_enable(true)
     end
   end
 
   describe 'Test standard installation with older puppetdb' do
-    let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42', :puppetdbversion => 1.32 } }
+    let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42', :puppetdbversion => '1.32', :operatingsystemrelease => '6.6' } }
 
     it { should contain_package('puppetdb').with_ensure('present') }
     it { should contain_service('puppetdb').with_ensure('running') }
